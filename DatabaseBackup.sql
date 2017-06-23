6a508e6032e2b0a6d2001634b65d2e2b4ebd498f
Brent Ozar <brento@brentozar.com>
2017-06-23 10:22:00 -0500

    #12 DatabaseBackup - new backup escalation option for MSDB
    If @ChangeBackupType = ‘MSDB’, look in MSDB for an existing full backup.

--- DatabaseBackup.sql
+++ DatabaseBackup.sql
@@ -711,7 +711,7 @@ BEGIN
     SET @Error = @@ERROR
   END
 
-  IF @ChangeBackupType NOT IN ('Y','N') OR @ChangeBackupType IS NULL
+  IF @ChangeBackupType NOT IN ('Y','N','MSDB') OR @ChangeBackupType IS NULL
   BEGIN
     SET @ErrorMessage = 'The value for the parameter @ChangeBackupType is not supported.' + CHAR(13) + CHAR(10) + ' '
     RAISERROR(@ErrorMessage,16,1) WITH NOWAIT
@@ -994,6 +994,20 @@ BEGIN
         SET @CurrentBackupType = 'FULL'
       END
     END
+
+    /* 
+      Escalate to full backups if no full is found in MSDB.
+      More info: https://github.com/olahallengren/sql-server-maintenance-solution/issues/12 
+    */
+    IF @ChangeBackupType = 'MSDB'
+    BEGIN
+      IF @CurrentBackupType = 'LOG' AND DATABASEPROPERTYEX(@CurrentDatabaseName,'Recovery') <> 'SIMPLE' AND @CurrentDatabaseName <> 'master'
+          AND NOT EXISTS (SELECT * FROM msdb.dbo.backupset WHERE [database_name] = @CurrentDatabaseName AND type = 'D' AND backup_finish_date IS NOT NULL)
+      BEGIN
+        SET @CurrentBackupType = 'FULL'
+      END
+    END
+
 
     IF @CurrentBackupType = 'LOG' AND (@CleanupTime IS NOT NULL OR @MirrorCleanupTime IS NOT NULL)
     BEGIN
