952b939af6753a0d517d1b2b3990b6e292365673
Ola Hallengren <ola@hallengren.com>
2018-04-01 15:33:19 +0200

    Add files via upload

--- CommandLog.sql
+++ CommandLog.sql
@@ -2,25 +2,30 @@
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-SET ANSI_PADDING ON
-GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CommandLog]') AND type in (N'U'))
+BEGIN
 CREATE TABLE [dbo].[CommandLog](
-[ID] int IDENTITY(1,1) NOT NULL CONSTRAINT [PK_CommandLog] PRIMARY KEY CLUSTERED,
-[DatabaseName] sysname NULL,
-[SchemaName] sysname NULL,
-[ObjectName] sysname NULL,
-[ObjectType] char(2) NULL,
-[IndexName] sysname NULL,
-[IndexType] tinyint NULL,
-[StatisticsName] sysname NULL,
-[PartitionNumber] int NULL,
-[ExtendedInfo] xml NULL,
-[Command] nvarchar(max) NOT NULL,
-[CommandType] nvarchar(60) NOT NULL,
-[StartTime] datetime NOT NULL,
-[EndTime] datetime NULL,
-[ErrorNumber] int NULL,
-[ErrorMessage] nvarchar(max) NULL
+  [ID] [int] IDENTITY(1,1) NOT NULL,
+  [DatabaseName] [sysname] NULL,
+  [SchemaName] [sysname] NULL,
+  [ObjectName] [sysname] NULL,
+  [ObjectType] [char](2) NULL,
+  [IndexName] [sysname] NULL,
+  [IndexType] [tinyint] NULL,
+  [StatisticsName] [sysname] NULL,
+  [PartitionNumber] [int] NULL,
+  [ExtendedInfo] [xml] NULL,
+  [Command] [nvarchar](max) NOT NULL,
+  [CommandType] [nvarchar](60) NOT NULL,
+  [StartTime] [datetime] NOT NULL,
+  [EndTime] [datetime] NULL,
+  [ErrorNumber] [int] NULL,
+  [ErrorMessage] [nvarchar](max) NULL,
+ CONSTRAINT [PK_CommandLog] PRIMARY KEY CLUSTERED
+(
+  [ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 )
+END
 GO
 
--- MaintenanceSolution.sql
+++ MaintenanceSolution.sql
@@ -88,26 +88,34 @@ VALUES('DatabaseName', DB_NAME(DB_ID()))
 
 INSERT INTO #Config ([Name], [Value])
 VALUES('Error', CAST(@Error AS nvarchar))
-
-IF OBJECT_ID('[dbo].[CommandLog]') IS NULL AND OBJECT_ID('[dbo].[PK_CommandLog]') IS NULL
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CommandLog]') AND type in (N'U'))
 BEGIN
 CREATE TABLE [dbo].[CommandLog](
-[ID] int IDENTITY(1,1) NOT NULL CONSTRAINT [PK_CommandLog] PRIMARY KEY CLUSTERED,
-[DatabaseName] sysname NULL,
-[SchemaName] sysname NULL,
-[ObjectName] sysname NULL,
-[ObjectType] char(2) NULL,
-[IndexName] sysname NULL,
-[IndexType] tinyint NULL,
-[StatisticsName] sysname NULL,
-[PartitionNumber] int NULL,
-[ExtendedInfo] xml NULL,
-[Command] nvarchar(max) NOT NULL,
-[CommandType] nvarchar(60) NOT NULL,
-[StartTime] datetime NOT NULL,
-[EndTime] datetime NULL,
-[ErrorNumber] int NULL,
-[ErrorMessage] nvarchar(max) NULL
+  [ID] [int] IDENTITY(1,1) NOT NULL,
+  [DatabaseName] [sysname] NULL,
+  [SchemaName] [sysname] NULL,
+  [ObjectName] [sysname] NULL,
+  [ObjectType] [char](2) NULL,
+  [IndexName] [sysname] NULL,
+  [IndexType] [tinyint] NULL,
+  [StatisticsName] [sysname] NULL,
+  [PartitionNumber] [int] NULL,
+  [ExtendedInfo] [xml] NULL,
+  [Command] [nvarchar](max) NOT NULL,
+  [CommandType] [nvarchar](60) NOT NULL,
+  [StartTime] [datetime] NOT NULL,
+  [EndTime] [datetime] NULL,
+  [ErrorNumber] [int] NULL,
+  [ErrorMessage] [nvarchar](max) NULL,
+ CONSTRAINT [PK_CommandLog] PRIMARY KEY CLUSTERED
+(
+  [ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 )
 END
 GO
