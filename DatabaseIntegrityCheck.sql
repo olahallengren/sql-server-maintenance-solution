cbc018d45601c93f4c32c37bfeee7384599cc1a0
Ola Hallengren <ola@hallengren.com>
2025-02-08 17:06:06 +0100

    Add files via upload

--- DatabaseIntegrityCheck.sql
+++ DatabaseIntegrityCheck.sql
@@ -40,7 +40,7 @@ BEGIN
   --// Source:  https://ola.hallengren.com                                                        //--
   --// License: https://ola.hallengren.com/license.html                                           //--
   --// GitHub:  https://github.com/olahallengren/sql-server-maintenance-solution                  //--
-  --// Version: 2025-02-08 16:16:38                                                               //--
+  --// Version: 2025-02-08 16:52:38                                                               //--
   ----------------------------------------------------------------------------------------------------
 
   SET NOCOUNT ON
