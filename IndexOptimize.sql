79459068d98defff3d00da257e4177f67745c585
Ola Hallengren <ola@hallengren.com>
2025-02-15 18:05:16 +0100

    Add files via upload

--- IndexOptimize.sql
+++ IndexOptimize.sql
@@ -54,7 +54,7 @@ BEGIN
   --// Source:  https://ola.hallengren.com                                                        //--
   --// License: https://ola.hallengren.com/license.html                                           //--
   --// GitHub:  https://github.com/olahallengren/sql-server-maintenance-solution                  //--
-  --// Version: 2025-02-12 21:17:38                                                               //--
+  --// Version: 2025-02-15 17:41:50                                                               //--
   ----------------------------------------------------------------------------------------------------
 
   SET NOCOUNT ON
@@ -1491,7 +1491,8 @@ BEGIN
     BEGIN
       SELECT @CurrentDatabaseMirroringRole = UPPER(mirroring_role_desc)
       FROM sys.database_mirroring
-      WHERE database_id = DB_ID(@CurrentDatabaseName)
+      INNER JOIN sys.databases databases ON database_mirroring.database_id = databases.database_id
+      WHERE databases.[name] = @CurrentDatabaseName
     END
 
     IF @CurrentAvailabilityGroup IS NOT NULL
