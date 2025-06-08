888986b91ed9de7fce1f2d8f7bacb76950e59c7f
Ola Hallengren <ola@hallengren.com>
2025-06-08 20:42:47 +0200

    Add files via upload

--- CommandExecute.sql
+++ CommandExecute.sql
@@ -36,7 +36,7 @@ BEGIN
   --// Source:  https://ola.hallengren.com                                                        //--
   --// License: https://ola.hallengren.com/license.html                                           //--
   --// GitHub:  https://github.com/olahallengren/sql-server-maintenance-solution                  //--
-  --// Version: 2025-06-08 12:16:23                                                               //--
+  --// Version: 2025-06-08 20:41:55                                                               //--
   ----------------------------------------------------------------------------------------------------
 
   SET NOCOUNT ON
