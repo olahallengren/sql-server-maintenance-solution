1ea5748fc9b07e5800f2aa135a3116f18bd27281
Ola Hallengren <ola@hallengren.com>
2026-07-21 19:00:54 +0200

    Update CommandExecute.sql

--- CommandExecute.sql
+++ CommandExecute.sql
@@ -2,7 +2,7 @@
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CommandExecute]') AND type in (N'P', N'PC'))
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.CommandExecute') AND type in (N'P', N'PC'))
 BEGIN
 EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[CommandExecute] AS'
 END
