0fe22a850242b88cdb00d2c75f958bf8f0dbd905
Ola Hallengren <ola@hallengren.com>
2026-07-22 21:58:16 +0200

    Add files via upload

--- DatabaseBackup.sql
+++ DatabaseBackup.sql
@@ -94,7 +94,7 @@ BEGIN
   --// Source:  https://ola.hallengren.com                                                        //--
   --// License: https://ola.hallengren.com/license.html                                           //--
   --// GitHub:  https://github.com/olahallengren/sql-server-maintenance-solution                  //--
-  --// Version: 2026-07-22 20:03:34                                                               //--
+  --// Version: 2026-07-22 21:56:04                                                               //--
   ----------------------------------------------------------------------------------------------------
 
   SET NOCOUNT ON
