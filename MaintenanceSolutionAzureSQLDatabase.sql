423a5b175b1f2a54926de49564d77a6a3998cd0f
Ola Hallengren <ola@hallengren.com>
2026-08-23 12:40:35 +0200

    Add files via upload

--- MaintenanceSolutionAzureSQLDatabase.sql
+++ MaintenanceSolutionAzureSQLDatabase.sql
@@ -9,7 +9,7 @@ License: https://ola.hallengren.com/license.html
 
 GitHub: https://github.com/olahallengren/sql-server-maintenance-solution
 
-Version: 2026-08-17 22:22:29
+Version: 2026-08-23 12:39:41
 
 You can contact me by e-mail at ola@hallengren.com.
 
@@ -88,7 +88,7 @@ BEGIN
   --// Source:  https://ola.hallengren.com                                                        //--
   --// License: https://ola.hallengren.com/license.html                                           //--
   --// GitHub:  https://github.com/olahallengren/sql-server-maintenance-solution                  //--
-  --// Version: 2026-08-17 22:22:29                                                               //--
+  --// Version: 2026-08-23 12:39:41                                                               //--
   ----------------------------------------------------------------------------------------------------
 
   SET NOCOUNT ON
@@ -394,7 +394,7 @@ BEGIN
   --// Source:  https://ola.hallengren.com                                                        //--
   --// License: https://ola.hallengren.com/license.html                                           //--
   --// GitHub:  https://github.com/olahallengren/sql-server-maintenance-solution                  //--
-  --// Version: 2026-08-17 22:22:29                                                               //--
+  --// Version: 2026-08-23 12:39:41                                                               //--
   ----------------------------------------------------------------------------------------------------
 
   SET NOCOUNT ON
@@ -2441,7 +2441,7 @@ BEGIN
   --// Source:  https://ola.hallengren.com                                                        //--
   --// License: https://ola.hallengren.com/license.html                                           //--
   --// GitHub:  https://github.com/olahallengren/sql-server-maintenance-solution                  //--
-  --// Version: 2026-08-17 22:22:29                                                               //--
+  --// Version: 2026-08-23 12:39:41                                                               //--
   ----------------------------------------------------------------------------------------------------
 
   SET NOCOUNT ON
